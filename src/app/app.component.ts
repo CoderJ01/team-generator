4ccb9a2045f5df02003e47fe3fad3a99ad49d1e9
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-08 15:30:32 -0800

    attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,20 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   newMemberName = ''
   members: string[] = [];
+  errorMessage = ''
 
   onInput(member: string) {
     this.newMemberName = member;
   }
 
   addMember() {
+    if(!this.newMemberName) {
+      this.errorMessage = 'Name can\'t be empty!';
+      return;
+    }
+
+    this.errorMessage = '';
     this.members.push(this.newMemberName);
+    this.newMemberName = '';
   }
 }
